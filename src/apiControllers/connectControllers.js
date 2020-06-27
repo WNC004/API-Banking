72def88aef9377821953943d6bb6c330de0961df
tri <thanhtri7637@gmail.com>
2020-06-27 15:08:49 +0700

    Revert "test api transfer pgp"
    This reverts commit 75a5b27bdcb292a617ec5b1e512de7c90ce83974.

--- src/apiControllers/connectControllers.js
+++ src/apiControllers/connectControllers.js
@@ -32,7 +32,6 @@ router.post("/PGP/users", verifyPGP , async (req,res) => {
     const accNumber = req.body.accountID;
     // newBalance = số dư cũ + tiền cần nạp;
     const newBalance = req.body.newBalance;
-    console.log(req.body);
 
     const payAccEntity = {
     accNumber,
@@ -51,7 +50,6 @@ router.post("/PGP/users", verifyPGP , async (req,res) => {
     .catch(err => {
         console.log(err);
         res.statusCode = 500;
-        console.log(req.body);
         res.end("View error log on console");
     });
 });

75a5b27bdcb292a617ec5b1e512de7c90ce83974
tri <thanhtri7637@gmail.com>
2020-06-27 15:08:29 +0700

    test api transfer pgp

--- src/apiControllers/connectControllers.js
+++ src/apiControllers/connectControllers.js
@@ -32,6 +32,7 @@ router.post("/PGP/users", verifyPGP , async (req,res) => {
     const accNumber = req.body.accountID;
     // newBalance = số dư cũ + tiền cần nạp;
     const newBalance = req.body.newBalance;
+    console.log(req.body);
 
     const payAccEntity = {
     accNumber,
@@ -50,6 +51,7 @@ router.post("/PGP/users", verifyPGP , async (req,res) => {
     .catch(err => {
         console.log(err);
         res.statusCode = 500;
+        console.log(req.body);
         res.end("View error log on console");
     });
 });
